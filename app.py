68c0bde05b3d9ab5f4f95c204f66f3da37437a3e
nstprtko <0never.mind2610@gmail.com>
2025-05-12 17:47:19 +0200

    2025-05-12 PA

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Hello world!"
+@app.route("/<name>")
+def index(name="World"):
+    return render_template("index.html", name=name)
+
+
